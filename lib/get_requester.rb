91915f4df2b546f63dda02321ea5979a9d8b5358
Lyn <lynfinman@gmail.com>
2022-07-10 23:08:51 -0500

    successful test

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  def initialize(request_URL)
+    @url = request_URL
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
